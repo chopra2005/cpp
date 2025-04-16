f2d663f88cb81009f0a23c33863643bcb5ce2f97
chopra2005 <choprabhay531@gmail.com>
2025-04-16 13:02:38 +0530

    ok

---
+++ si.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int p = 100;
+    int r = 20;
+    int t = 5;
+    int si = (p * r * t)/100;
+    cout<<si<<endl;
+    return 0;
+}
+ 
+
---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout<< "abhay";
+    
+    return 0;
+}
