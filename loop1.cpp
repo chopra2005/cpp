e91269a75c0f93e7d7ecf0db90bf799b1695caed
chopra2005 <choprabhay531@gmail.com>
2025-04-17 13:03:14 +0530

    ok

---
+++ loop1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    for (int round=1;round<=1000;round--){
+        cout<<"hello"<<endl;
+    }
+}
---
+++ loop2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    for(int round =2; round<=100; round++){
+        cout<<round<<endl;
+    }
+}
