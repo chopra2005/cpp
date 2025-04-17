e6eadaf30a6ab2e0e9199adc4ec08b8b8a312bc6
chopra2005 <choprabhay531@gmail.com>
2025-04-17 10:59:55 +0530

    ok

---
+++ condition.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n=10;
+    if(n==10){
+        cout<<"n is equal to 10"<<endl;
+        cout<<"university"<<endl;
+        cout<<n*n*n<<endl;
+    }
+    else{
+        cout<<"n is not equal to 10"<<endl;
+        cout<<n/2<<endl;
+        n=0;
+        cout<<n<<endl;
+    }
+}
